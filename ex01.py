a4704bcfdd9baaa746392899af4f2aa29d387b00
Heloisa Costa <heloisacosta2005@gmail.com>
2025-04-01 14:00:16 -0300

    ex01

---
+++ ex01.py
@@ -0,0 +1,5 @@
+nome = input("Qual seu nome: ")
+idade = int(input("Diga sua idade: "))
+salario = float(input("Diga seu salário: "))
+
+print(f"Seu nome é {nome}, você tem {idade} anos e seu salário é R${salario}")
