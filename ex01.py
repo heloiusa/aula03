30e8cc69de427be1ebd503870af447e05cf2cd50
Heloisa Costa <heloisacosta2005@gmail.com>
2025-04-01 14:25:06 -0300

    ajustei uns erros

--- ex01.py
+++ ex01.py
@@ -1,10 +1,10 @@
 nome = input("Qual seu nome: ")
 idade = int(input("Diga sua idade: "))
 salario = float(input("Diga seu salário: "))
-porcentagem = float(input("Digite o valor de aumento de salário: "))
+percentual = float(input("Digite o valor de aumento de salário: "))
 
-percentual = (salario * porcentagem)/100
-valorfinal = salario + percentual
+aumento = (salario * percentual)/100
+novosalario = salario + aumento
 
-print(f"Seu nome é {nome}, você tem {idade} anos e seu salário é R${salario}, devido ao aumento de {porcentagem},"
-      f" o seu novo salário ficou R${valorfinal}.")
+print(f"Seu nome é {nome}, você tem {idade} anos e seu salário é R${salario}, devido ao aumento de {percentual},"
+      f" o seu novo salário ficou R${novosalario}.")
