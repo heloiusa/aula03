b45aa760a53f3e04f59f80f8bbbe7436b91e9d55
Heloisa Costa <heloisacosta2005@gmail.com>
2025-04-01 15:39:55 -0300

    numero maior

---
+++ ex04.py
@@ -0,0 +1,7 @@
+num1 = int(input("Digite o 1º número: "))
+num2 = int(input("Digite o 2º número: "))
+
+if num1 > num2:
+    print(f"O {num1} é o maior número.")
+else:
+    print(f"O {num2} é o maior número.")
