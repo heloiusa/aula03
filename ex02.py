eebb5d616f25c91fc5f76b20c03bd9f85a260e3c
Heloisa Costa <heloisacosta2005@gmail.com>
2025-04-01 14:42:16 -0300

    calculo de 4 operacao

---
+++ ex02.py
@@ -0,0 +1,10 @@
+num1 = int(input("Digite o 1º número: "))
+num2 = int(input("Digite o 2º número: "))
+
+soma = num1 + num2
+sub = num1 - num2
+div = num1 / num2
+mult = num1 * num2
+
+print(f"As 4 operações entre os números {num1} e {num2} são: a soma é {soma}, a subtração é {sub}, a divisão é {div} e "
+      f"a multiplicação é {mult}.")
